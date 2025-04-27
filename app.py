d233887a90eac712b95ef0450106a38092595ea1
nilesh.damodare@3pillarglobal.com <24380037niL*>
2025-04-27 19:14:44 +0530

    Add Item ID field to To-Do form (re-commit)

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, request
-from pymongo import MongoClient  # only if you're doing the backend too
+from flask import request
+from pymongo import MongoClient
 
-app = Flask(__name__)
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["todo_db"]
+collection = db["items"]
 
-@app.route("/todo")
-def todo_page():
-    return render_template("todo.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo_item():
+    item_name = request.form.get("itemName")
+    item_desc = request.form.get("itemDescription")
+    if item_name and item_desc:
+        collection.insert_one({
+            "itemName": item_name,
+            "itemDescription": item_desc
+        })
+        return "Item submitted successfully!"
+    else:
+        return "Missing fields", 400
